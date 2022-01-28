794530985b586d578591fd18959862b3db1b61e1
saidmsaif <saidmsaiff@gmail.com>
2022-01-28 23:11:34 +0600

    responsive design added and uninstall oldage packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, useEffect } from 'react'
+import Footer from './components/Footer';
 import Header from './components/Header';
 import LeftPanel from './components/LeftPanel';
 import RightPanel from './components/RightPanel';
@@ -25,29 +26,33 @@ export class App extends Component {
     render() {
         return (
             <>
-                <Header />
-                
-                <div className="w-10/12 mx-auto grid grid-cols-5">
-                    <div className="py-6 px-4 my-6 bg-white">
-                        <LeftPanel boxShadowProps={this.boxShadowProps}/>
-                    </div>
-                    <div className="flex items-center justify-center my-6 px-4 col-span-3">
-                        <div style={this.state}>
-                            {/* Middle Box */}
+                <div className='flex flex-col min-h-screen justify-between'>
+                    <Header />
+                    
+                    <div className="flex-grow w-10/12 mx-auto grid grid-cols-1 2xl:grid-cols-5">
+                        <div className="py-6 px-4 my-6 bg-white">
+                            <LeftPanel boxShadowProps={this.boxShadowProps}/>
+                        </div>
+                        <div className="flex items-center justify-center my-6 px-4 col-span-3">
+                            <div style={this.state}>
+                                {/* Middle Box */}
 
-                            <pre class="codeblock">
-        <div class="codeDiv">
-<svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"></circle><circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"></circle><circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"></circle></g></svg>
-<code class="code language-javascript">
-box-shadow: {this.state.boxShadow + ';'}
-</code>
-        </div>
-    </pre>
+                                <pre class="codeblock">
+            <div class="codeDiv">
+    <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"></circle><circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"></circle><circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"></circle></g></svg>
+    <code class="code language-javascript">
+    box-shadow: {this.state.boxShadow + ';'}
+    </code>
+            </div>
+        </pre>
+                            </div>
+                        </div>
+                        <div className="py-6 px-4 my-6 bg-white">
+                            <RightPanel boxProps={this.boxProps}/>
                         </div>
                     </div>
-                    <div className="py-6 px-4 my-6 bg-white">
-                        <RightPanel boxProps={this.boxProps}/>
-                    </div>
+
+                    <Footer />
                 </div>
             </>
         )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { RiExternalLinkLine, RiGithubFill } from "react-icons/ri";
-import { Link } from 'react-router-dom'
+import { RiExternalLinkLine } from "react-icons/ri";
 
 export class Header extends Component {
   render() {
     return <>
-        <div className="py-4 bg-cyan-600">
-            <div className="w-10/12 mx-auto flex justify-between items-center">
-                <h1 className='text-white font-bold text-2xl'>Box Shadow Generator</h1>
+        <div className="2xl:h-72 py-12 2xl:py-0 flex bg-cyan-600">
+            <div className="w-10/12 mx-auto flex flex-col 2xl:flex-row justify-between items-center">
+                <h1 className='text-white text-4xl mb-4 2xl:mb-0 xl:text-6xl font-thin'>Box Shadow Generator</h1>
 
                 <div className='flex justify-end'>
                     <a href='https://github.com/saifullahsiddique/' target='_blank' className='bg-gray-100 hover:bg-gray-300 transition duration-300 flex gap-2 items-center px-3 py-1 rounded-sm text-sm uppercase' >
