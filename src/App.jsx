5d19fd91e3b246fa6f026390214cad4228ce0c49
saidmsaif <saidmsaiff@gmail.com>
2022-01-28 09:12:27 +0600

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Header from './components/Header';
 
 export class App extends Component {
     render() {
         return (
-            <div>
-                <h1 className='underline'>Lorem ipsum dolor sit amet consectetur.</h1>
-            </div>
+            <>
+                <Header />
+            </>
         )
     }
 }
