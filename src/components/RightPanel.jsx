cb2573baaf5e9a83e9a14009c34a7ca0fe347fa2
saidmsaif <saidmsaiff@gmail.com>
2022-01-28 14:46:40 +0600

    modify design

--- src/components/RightPanel.jsx
+++ src/components/RightPanel.jsx
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 export class RightPanel extends Component {
     render() {
         return <>
+            <h6>Box Properties</h6>
+            <hr />
+
             <div className="my-6">
-                <h6>Box Properties</h6>
+                
             </div>
         </>;
     }
