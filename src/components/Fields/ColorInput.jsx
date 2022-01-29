15f763ce698085f3eda0a68c6fd3d45da4ccf6d2
saidmsaif <saidmsaiff@gmail.com>
2022-01-29 19:52:02 +0600

    added color and range input components

---
+++ src/components/Fields/ColorInput.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class ColorInput extends Component {
+    render() {
+        const [label, type, name, cssValue]    =   this.props
+        return <>
+            <div className='mt-6'>
+                <label htmlFor={name} className='w-full flex justify-between'>
+                    <span>{label}</span>
+                    <span>{cssValue}</span>
+                </label>
+                <input type={type} name={name} id={name} value={cssValue} className='mt-4 w-full' />
+            </div>
+        </>;
+    }
+}
+
+export default ColorInput;
--- src/components/Fields/RangeInput.jsx
+++ src/components/Fields/RangeInput.jsx
@@ -7,7 +7,7 @@ export class RangeInput extends Component {
             <div className='mt-6'>
                 <label htmlFor={name} className='w-full flex justify-between'>
                     <span>{label}</span>
-                    <span>{cssValue} rem</span>
+                    <span>{cssValue}</span>
                 </label>
                 <input type={type} name={name} id={name} value={cssValue} className='mt-4 w-full' min={min} max={max} />
             </div>
