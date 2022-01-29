8745c12d519c5fffd0b6c3914f433c2881a4c5e8
saidmsaif <saidmsaiff@gmail.com>
2022-01-29 22:20:05 +0600

    modify range input component

--- src/components/Fields/RangeInput.jsx
+++ src/components/Fields/RangeInput.jsx
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 
 export class RangeInput extends Component {
     render() {
-        const [label, type, name, cssValue, min, max]    =   this.props
+        const {label, type, name, cssValue, min, max}    =   this.props
+
         return <>
             <div className='mt-6'>
                 <label htmlFor={name} className='w-full flex justify-between'>
                     <span>{label}</span>
                     <span>{cssValue}</span>
                 </label>
-                <input type={type} name={name} id={name} value={cssValue} className='mt-4 w-full' min={min} max={max} />
+                <input type={type} name={name} id={name} value={cssValue.replace(/[^\d-]/g, '')} className='mt-4 w-full' min={min} max={max} />
             </div>
         </>;
     }
