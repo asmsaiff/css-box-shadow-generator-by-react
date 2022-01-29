cba92b97c8f39a692534783e8ec8a5b256aa7b2e
saidmsaif <saidmsaiff@gmail.com>
2022-01-29 22:20:34 +0600

    modify footer credit and add link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export class Footer extends Component {
     return <>
         <div className="py-4 bg-cyan-600">
             <div className="w-10/12 mx-auto flex justify-between items-center">
-                <h1 className='text-white font-thin text-2xl w-max mx-auto'>Form - Saifullah Saif</h1>
+                <h1 className='text-white font-thin text-2xl w-max mx-auto'>Developed By - <a href="https://saifullah.co/" target="_blank" className='hover:underline'>Saifullah Siddique</a></h1>
             </div>
         </div>
     </>;
